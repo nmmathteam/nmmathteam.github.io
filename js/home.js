4e9c5cae0d71b831f4432894cdf7e00a2f50a3d6
sjchoi25 <69376044+sjchoi25@users.noreply.github.com>
2024-02-11 22:12:09 -0700

    Sponsors

--- js/home.js
+++ js/home.js
@@ -10,7 +10,7 @@ function $(e){return document.querySelector(e)}const nav=`
 <ul>
     <li><a href="#">About us</a></li>
     <li><a href="#">Our Team</a></li>
-    <li><a href="/sponsors">Sponsors</a></li>
+    <li><a href="#">Sponsors</a></li>
     <li><a href="/calendar">Calendar</a></li>
     <li><a href="https://forms.gle/efdDBimGox1kPukQ7" target="_blank" id="join">Join!</a></li>
 </ul>
@@ -24,4 +24,4 @@ function $(e){return document.querySelector(e)}const nav=`
     <a href="/">Home</a>
     <a href="/">Link 1</a>
 </div>
-`;$("nav").innerHTML=nav,$("footer").innerHTML=footer,window.onscroll=()=>{window.scrollY>=10?$("nav").classList.add("collapsed-nav"):window.scrollY<=0&&$("nav").classList.remove("collapsed-nav")},$("nav ul li:nth-of-type(1)").addEventListener("click",e=>{e.preventDefault(),window.scrollTo({top:$(".cards-container").getBoundingClientRect().top+window.pageYOffset-60,behavior:"smooth"})}),$("nav ul li:nth-of-type(2)").addEventListener("click",e=>{e.preventDefault(),window.scrollTo({top:$(".wave").getBoundingClientRect().top+window.pageYOffset-60,behavior:"smooth"})});const searchParams=new URLSearchParams(window.location.search);searchParams.has("e")&&document.addEventListener("DOMContentLoaded",e=>{window.scrollTo({top:$(`.${searchParams.get("e")}`).getBoundingClientRect().top+window.pageYOffset-60,behavior:"smooth"})})
+`;$("nav").innerHTML=nav,$("footer").innerHTML=footer,window.onscroll=()=>{window.scrollY>=10?$("nav").classList.add("collapsed-nav"):window.scrollY<=0&&$("nav").classList.remove("collapsed-nav")};function scroll(e,t){$(`nav ul li:nth-of-type(${e})`).addEventListener("click",e=>{e.preventDefault(),window.scrollTo({top:$(t).getBoundingClientRect().top+window.pageYOffset-60,behavior:"smooth"})})}scroll(1,".cards-container"),scroll(2,".wave"),scroll(3,".sponsors");const searchParams=new URLSearchParams(window.location.search);searchParams.has("e")&&document.addEventListener("DOMContentLoaded",e=>{window.scrollTo({top:$(`.${searchParams.get("e")}`).getBoundingClientRect().top+window.pageYOffset-60,behavior:"smooth"})})
