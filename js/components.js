57fec25a1852be47fedcb1c2bfdcbd1f8041d63f
Sean <69376044+sjchoi25@users.noreply.github.com>
2024-11-16 19:02:57 -0700

    handouts!

--- js/components.js
+++ js/components.js
@@ -1,6 +1,6 @@
-function $(e){return document.querySelector(e)}const nav=`
+function $(a){return document.querySelector(a)}$('nav').innerHTML=`
 <a href="/">
-    <img src="/img/NMMT.png" alt="Logo" />
+    <img src="img/NMMT.png" alt="Logo" />
 </a>
 <div id="hamburger" onclick="this.classList.toggle('open');document.querySelector('nav ul').classList.toggle('open');">
     <div></div>
@@ -12,9 +12,10 @@ function $(e){return document.querySelector(e)}const nav=`
     <li><a href="/?e=wave">Our Team</a></li>
     <li><a href="/?e=sponsors">Sponsors</a></li>
     <li><a href="/calendar">Calendar</a></li>
+    <li><a href="/handouts">Handouts</a></li>
     <li><a href="https://forms.gle/efdDBimGox1kPukQ7" target="_blank" id="join">Join!</a></li>
 </ul>
-`,footer=`
+`;$('footer').innerHTML=`
 <div>
     <div id="copyright">&copy; New Mexico Math Team</div>
     <div id="madebyme">Made by Sean Choi for NMMT</div>
@@ -24,4 +25,4 @@ function $(e){return document.querySelector(e)}const nav=`
     <a href="/">Home</a>
     <a href="/">Link 1</a>
 </div>
-`;$("nav").innerHTML=nav,$("footer").innerHTML=footer,window.onscroll=()=>{window.scrollY>=10?$("nav").classList.add("collapsed-nav"):window.scrollY<=0&&$("nav").classList.remove("collapsed-nav")}
+`;window.onscroll=()=>{if(window.scrollY>=10)$('nav').classList.add('collapsed-nav');else window.scrollY<=0&&$('nav').classList.remove('collapsed-nav')};
--- js/home.js
+++ js/home.js
@@ -1,6 +1,6 @@
-function $(e){return document.querySelector(e)}const nav=`
+function $(a){return document.querySelector(a)}$('nav').innerHTML=`
 <a href="/">
-    <img src="/img/NMMT.png" alt="Logo" />
+    <img src="img/NMMT.png" alt="Logo" />
 </a>
 <div id="hamburger" onclick="this.classList.toggle('open');document.querySelector('nav ul').classList.toggle('open');">
     <div></div>
@@ -12,9 +12,10 @@ function $(e){return document.querySelector(e)}const nav=`
     <li><a href="#">Our Team</a></li>
     <li><a href="#">Sponsors</a></li>
     <li><a href="/calendar">Calendar</a></li>
+    <li><a href="/handouts">Handouts</a></li>
     <li><a href="https://forms.gle/efdDBimGox1kPukQ7" target="_blank" id="join">Join!</a></li>
 </ul>
-`,footer=`
+`;$('footer').innerHTML=`
 <div>
     <div id="copyright">&copy; New Mexico Math Team</div>
     <div id="madebyme">Made by Sean Choi for NMMT</div>
@@ -24,4 +25,4 @@ function $(e){return document.querySelector(e)}const nav=`
     <a href="/">Home</a>
     <a href="/">Link 1</a>
 </div>
-`;$("nav").innerHTML=nav,$("footer").innerHTML=footer,window.onscroll=()=>{window.scrollY>=10?$("nav").classList.add("collapsed-nav"):window.scrollY<=0&&$("nav").classList.remove("collapsed-nav")};function scroll(e,t){$(`nav ul li:nth-of-type(${e})`).addEventListener("click",e=>{e.preventDefault(),window.scrollTo({top:$(t).getBoundingClientRect().top+window.pageYOffset-60,behavior:"smooth"})})}scroll(1,".cards-container"),scroll(2,".wave"),scroll(3,".sponsors");const searchParams=new URLSearchParams(window.location.search);searchParams.has("e")&&document.addEventListener("DOMContentLoaded",e=>{window.scrollTo({top:$(`.${searchParams.get("e")}`).getBoundingClientRect().top+window.pageYOffset-60,behavior:"smooth"})})
+`;window.onscroll=()=>{if(window.scrollY>=10)$('nav').classList.add('collapsed-nav');else window.scrollY<=0&&$('nav').classList.remove('collapsed-nav')};function b(A,_){$(`nav ul li:nth-of-type(${A})`).addEventListener('click',e=>{e.preventDefault();window.scrollTo({top:$(_).getBoundingClientRect().top+window.pageYOffset-60,behavior:'smooth'})})}b(1,'.cards-container');b(2,'.wave');b(3,'.sponsors');var c=new URLSearchParams(window.location.search);c.has('e')&&document.addEventListener('DOMContentLoaded',e=>window.scrollTo({top:$(`.${c.get('e')}`).getBoundingClientRect().top+window.pageYOffset-60,behavior:'smooth'}));
